73701bdb252852ba6c16b3b5387ca9b676f016f5
torik <riqibrekele@gmail.com>
2021-09-02 21:16:51 +0700

    FIX: validate token error inside extractJWT.ts

--- source/middleware/extractJWT.ts
+++ source/middleware/extractJWT.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 
 const NAMESPACE = "Auth - Middleware";
 
-const extractJWT = (req: Request, res: Response, next: NextFunction) => {
+const extractJWT = ((req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'Validating Token..');
 
     let token = req.headers.authorization?.split('-')[1];
@@ -20,6 +20,7 @@ const extractJWT = (req: Request, res: Response, next: NextFunction) => {
             }
             else {
                 res.locals.jwt = decoded;
+                next();
             }
         });
     }
@@ -28,6 +29,6 @@ const extractJWT = (req: Request, res: Response, next: NextFunction) => {
             message: "Unauthorized"
         });
     }
-};
+});
 
 export default extractJWT;
